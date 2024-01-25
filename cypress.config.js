a2e9a35b12cd9090198e37a74c4d0be8a8cabe3b
hannaustch <hannaustch@gmail.com>
2024-01-25 11:24:47 +0100

    update configs

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,15 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: 'cypress/e2e/*.spec',
   },
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  defaultCommandTimeout: 60000,
+  pageLoadTimeout: 60000,
+  requestTimeout: 30000,
+  chromeWebSecurity: false,
+  watchForFileChanges: true,
+  projectId: 'b738z9',
+  retries: 1,
 });
